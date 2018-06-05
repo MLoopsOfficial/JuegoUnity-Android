a3c3480f886821bb76114a3032a46184a0e188fc
MLoopsOfficial <31869894+MLoops@users.noreply.github.com>
2018-06-05 10:09:06 -0500

    TouchEvent

---
+++ ScaryMonsters-MobileVersion/Assets/Scripts/UI-Menus/PantallaInicioToque.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PantallaInicioToque : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			if (Input.GetTouch (0).phase == TouchPhase.Began) {
+				Debug.Log ("Tocaste la pantalla xD");
+			}
+			if(Input.GetTouch(0).phase == TouchPhase.Moved){
+				Debug.Log ("Moviendo Pantalla xD x2");
+			}
+			if(Input.GetTouch(0).phase == TouchPhase.Ended){
+				Debug.Log ("Dejaste de tocar Pantalla xD");
+			}
+		}
+	}
+}
