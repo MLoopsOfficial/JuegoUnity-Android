bab0ff6fb82b0ae5b9449dee52bed294c75655c2
MLoopsOfficial <31869894+MLoops@users.noreply.github.com>
2018-06-09 21:26:09 -0500

    Conexion con Mongo DB
    se establecio conexion con Mongo DB al fin xD
    
    Co-Authored-By: Diana <dianafg19@users.noreply.github.com>

--- ScaryMonsters-MobileVersion/Assets/Scripts/ConexionMongoDB.cs
+++ ScaryMonsters-MobileVersion/Assets/Scripts/ConexionMongoDB.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System; 
+using System.Collections;
+using System.Collections.Generic;  
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
 
 public class ConexionMongoDB : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+   string stringConexion = "mongodb://<moises>:<Dx9857*01>@ds161580.mlab.com:61580/scary-monstersbd";
+    void Start()
+    {
+        /*
+         * Establecer conexion local
+        
+        string stringConexion = "mongodb://<dbuser>:<dbpassword>@ds153890.mlab.com:53890/jugadores";
+       
+        var coleccionJugadores = database.GetCollection("vrshop");//Solo cuando uses la coleccion la pones xD
+         */
+        MongoClient client = new MongoClient(stringConexion);
+        var server = client.GetServer();
+        var bd = server.GetDatabase("scary-monstersbd");
+       
+
+        Debug.Log("Conexion Establecida con éxito");
+    }
 }
