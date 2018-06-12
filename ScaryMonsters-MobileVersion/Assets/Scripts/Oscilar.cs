390a80c64b98f23fbfecb8171ff99560b4f75c43
MLoopsOfficial <31869894+MLoops@users.noreply.github.com>
2018-06-12 12:26:39 -0500

    12-06-2018 2 commit
    ...
    
    Co-Authored-By: MLoops_Official <mloopsofficial@users.noreply.github.com>

---
+++ ScaryMonsters-MobileVersion/Assets/Scripts/Oscilar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilar : MonoBehaviour {
+
+	private Vector3 _startPosition;
+	public float multiplicador;
+	public float multiplicadorAmplitud;
+
+	// Use this for initialization
+	void Start () {
+		_startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = _startPosition + new Vector3(0.0f, (Mathf.Sin(Time.time*multiplicador))*multiplicadorAmplitud, 0.0f);
+	}
+}
