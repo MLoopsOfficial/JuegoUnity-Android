6f4f404349453b5b3eac45a0e564e803abbb5e45
MLoopsOfficial <31869894+MLoops@users.noreply.github.com>
2018-06-13 16:13:51 -0500

    13/06/2018
    In this Update, we added some blender models and their respective textures for testing

---
+++ ScaryMonsters-MobileVersion/Assets/Scripts/Item.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	public int valorItem=0;
+	public int multiplicadorPuntos = 0;
+	public bool rotarItem = true;
+	public bool oscilarItem = true;
+	private RotarObjeto scriptRotacion;
+	private Oscilar scriptOscilar;
+	// Use this for initialization
+	void Start () {
+		scriptRotacion = GetComponent<RotarObjeto> ();
+		scriptOscilar = GetComponent<Oscilar> ();
+		multiplicadorPuntos = valorItem * 10;
+		checarOsc ();
+		checarRot ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void checarRot()
+	{
+		if (rotarItem) {
+			scriptRotacion.enabled = true;
+		} else {
+			scriptRotacion.enabled = false;
+		}
+	}
+	void checarOsc()
+	{
+		if (oscilarItem) {
+			scriptOscilar.enabled = true;
+		} else {
+			scriptOscilar.enabled = false;
+		}
+	}
+}
---
+++ ScaryMonsters-MobileVersion/Assets/Scripts/RotarObjeto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotarObjeto : MonoBehaviour {
+
+	public float velX;
+	public float velY;
+	public float velZ;
+	// Use this for initialization
+	void Start () {
+		float nrmX = velX * Time.deltaTime;
+		float nrmY = velY * Time.deltaTime;
+		float nrmZ = velZ * Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (velX,velY,velZ);
+	}
+}
