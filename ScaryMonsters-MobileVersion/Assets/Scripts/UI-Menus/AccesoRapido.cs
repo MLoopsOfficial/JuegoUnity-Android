1acfc7a0d7034595036512db86aedcac04973821
MLoopsOfficial <31869894+MLoops@users.noreply.github.com>
2018-06-28 11:47:52 -0500

    28/06/2018-2
    NO SEEE X2

--- ScaryMonsters-MobileVersion/Assets/Scripts/UI-Menus/AccesoRapido.cs
+++ ScaryMonsters-MobileVersion/Assets/Scripts/UI-Menus/AccesoRapido.cs
@@ -8,6 +8,9 @@ public class AccesoRapido : MonoBehaviour {
 	public GameObject botonOpc;
 	public GameObject botonShop;
 
+	public bool homeActivo=true;
+	public bool opcActivo=true;
+	public bool shopActivo=true;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,6 +18,23 @@ public class AccesoRapido : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//Boton Home
+		if (homeActivo == true) {
+			botonHome.SetActive (true);
+		} else {
+			botonHome.SetActive (false);
+		}
+		//Boton Opciones
+		if (opcActivo == true) {
+			botonOpc.SetActive (true);
+		} else {
+			botonOpc.SetActive (false);
+		}
+		//Boton Tienda
+		if (shopActivo == true) {
+			botonShop.SetActive (true);
+		} else {
+			botonShop.SetActive (false);
+		}
 	}
 }
