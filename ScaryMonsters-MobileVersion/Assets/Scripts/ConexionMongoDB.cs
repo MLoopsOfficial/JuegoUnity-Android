cf4a10c01b9981da381b1f4ef7b9371d13686fe4
MLoopsOfficial <31869894+MLoops@users.noreply.github.com>
2018-07-23 19:38:34 -0500

    Conexion Exitosaaaaaaaaaa
    @dianafg19 pues ya ya esta

--- ScaryMonsters-MobileVersion/Assets/Scripts/ConexionMongoDB.cs
+++ ScaryMonsters-MobileVersion/Assets/Scripts/ConexionMongoDB.cs
@@ -2,6 +2,7 @@
 using System; 
 using System.Collections;
 using System.Collections.Generic;  
+using UnityEngine.UI;
 
 using MongoDB.Bson;
 using MongoDB.Driver;
@@ -10,24 +11,22 @@ using MongoDB.Driver.GridFS;
 using MongoDB.Driver.Linq;
 
 public class ConexionMongoDB : MonoBehaviour {
-
-    
-   string stringConexion = "mongodb://<moises>:<Dx9857*01>@ds161580.mlab.com:61580/scary-monstersbd";
+	
+	string msj = "Conexion Establecida con éxito";
+	string stringConexion = "mongodb://admin:a12345@ds147451.mlab.com:47451/scary-monsters";
+	public Text contenedorMsj;
     void Start()
-    {
-        /*
-         * Establecer conexion local
-        
-        string stringConexion = "mongodb://<dbuser>:<dbpassword>@ds153890.mlab.com:53890/jugadores";
-       
-        var coleccionJugadores = database.GetCollection("vrshop");//Solo cuando uses la coleccion la pones xD
-         */
-        MongoClient client = new MongoClient(stringConexion);
-        var server = client.GetServer();
-        var bd = server.GetDatabase("scary-monstersbd");
+	{
+		MongoClient client = new MongoClient (stringConexion);
+		var db = client.GetServer ();
+		var bd = db.GetDatabase ("scary-monsters");
+		var jugadores = bd.GetCollection ("jugadores");
 
-        Debug.Log("Conexion Establecida con éxito");
+		if (jugadores == null) {
+			contenedorMsj.text = "NOOOOOOOOOOOO";
+		} else {
+			contenedorMsj.text = msj;
+		}
 
-    }
-
+	}
 }
